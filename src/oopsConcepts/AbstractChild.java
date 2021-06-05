d5a6106f0fc338a9dec1dcfa935571f1496d3d2e
Tintu <tintulejosh@gmail.com>
2021-06-05 02:12:11 +0000

    Commit Abstraction

---
+++ src/oopsConcepts/AbstractChild.java
@@ -0,0 +1,13 @@
+package oopsConcepts;
+
+public class AbstractChild extends AbstractionExample {
+
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Insdie Method2");
+		
+	}
+
+}
---
+++ src/oopsConcepts/AbstractionExample.java
@@ -0,0 +1,12 @@
+package oopsConcepts;
+
+public abstract class AbstractionExample {
+	
+	public void method1() {
+		
+		System.out.println("Inside Method1");
+	}
+	
+	public abstract void method2();
+
+}
